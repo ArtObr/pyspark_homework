7c394317b70b5c275efb73927c749cf7475c9133
pump <pump@pump.pump>
2021-01-15 15:46:01 +0300

    HistoryProduct not fully done

--- homework/history_product/HistoryProduct.py
+++ homework/history_product/HistoryProduct.py
@@ -1,4 +1,5 @@
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import lit, asc
 
 
 class HistoryProduct:
@@ -12,9 +13,27 @@ class HistoryProduct:
      Result should contain all rows from both datasets and new column `meta`
      where status of each row should be provided ('not_changed', 'changed', 'inserted', 'deleted')
     """
-    def __init__(self, primary_keys=None):
-        pass
 
-    # ToDo: Implement history product
+    def __init__(self, primary_keys=None):
+        self.primary_keys = primary_keys or ['id', 'name', 'score']
+
     def get_history_product(self, old_dataframe: DataFrame, new_dataframe: DataFrame):
-        pass
+        # None filter
+        inserted = new_dataframe.join(old_dataframe, on=self.primary_keys, how='anti') \
+            .withColumn('meta', lit('inserted')).withColumn('priority', lit(1))
+        deleted = old_dataframe.join(new_dataframe, on=self.primary_keys, how='anti') \
+            .withColumn('meta', lit('deleted')).withColumn('priority', lit(0))
+        not_changed = new_dataframe.join(old_dataframe, on=['id', 'name', 'score']) \
+            .withColumn('meta', lit('not_changed')).withColumn('priority', lit(1))
+        changed = new_dataframe.join(inserted, on=self.primary_keys, how='anti') \
+            .join(not_changed, on=self.primary_keys, how='anti') \
+            .withColumn('meta', lit('changed')).withColumn('priority', lit(1))
+
+        inserted.show()
+        deleted.show()
+        not_changed.show()
+        changed.show()
+
+        return inserted.union(deleted).union(not_changed).union(changed) \
+            .sort(asc('id'), asc('priority')) \
+            .drop('priority')
